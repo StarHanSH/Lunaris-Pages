75f2262abe1b6417ce0cae8a109574947527fb42
星寒工作室 <starhanteam@163.com>
2025-07-06 20:22:09 +0800

    Update cardBasic.js

--- cardBasic.js
+++ cardBasic.js
@@ -5,9 +5,9 @@ export const cardBasic = {
   title: "基本信息",
   icon: "fas fa-info-circle",
   items: [
-    "成立于：2025.3.7",
-    "口号：传承 创新 团结 共享",
-    "领域：信息技术、视频创作、游戏、二次元",
-    "性质：非营利性创作团队"
+    "text",
+    "text",
+    "text",
+    "text"
   ]
 };
